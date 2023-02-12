ddbbce38a0277b1d43c7a5c6ee0cb1ffb916502d
Aubrey Loudenslager <aubrey.d.webster@gmail.com>
2023-02-12 15:00:40 -0700

    commit tree 05e3c2777059318def00208c5a0e523417af5dc8

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,9 +1,8 @@
 require ( './root.js' );
 
-
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO');
+    expect(shout('hello')).toEqual('HELLO'); 
   })
 })
 
